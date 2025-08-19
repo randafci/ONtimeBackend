a6d298ba7d117e96e4efc066e3fbf4b8eda89963
randafci0 <randafci0@gmail.com>
2025-08-19 16:25:45 +0300

    fix

---
+++ Project.Module.User/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnTime.ResponseHandler.Models;
+using OnTime.Services.DataTransferObject.AuthenticationDto;
+using OnTime.Services.Helpers;
+using OnTime.Services.Interfaces;
+using OnTime.User.Services.DTO;
+using OnTime.User.Services.Implementation;
+using OnTime.User.Services.Interfaces;
+using System.Net;
+
+namespace OnTime.User.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ApiControllerBase
+    {
+        #region fields
+        private readonly IAccountServices _authenticationService;
+        private readonly IHelpureService _helpureService;
+        #endregion
+
+        #region ctor
+        public AccountController(IAccountServices authenticationService, IHelpureService helpureService)
+        {
+            _authenticationService = authenticationService;
+            _helpureService = helpureService;
+        }
+        #endregion
+
+
+
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginInformation request)
+        {
+
+            var result = await _authenticationService.Login(request);
+
+            return ProcessResponse(result);
+
+        }
+
+        [Authorize]
+        [HttpGet("user-claims")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllUserClaims()
+        {
+            var result = await _authenticationService.GetRoleClaimsOnlyAsync();
+
+
+            return ProcessResponse(result);
+        }
+    }
+}
---
+++ Project.Module.User/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnTime.ResponseHandler.Models;
+using OnTime.Services.DataTransferObject.AuthenticationDto;
+using OnTime.Services.DataTransferObject.Customer;
+using OnTime.Services.Helpers;
+using OnTime.Services.Interfaces;
+namespace OnTime.Api.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ApiControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        // GET: api/customer
+        [HttpGet]
+        public async Task<IActionResult> GetAllCustomers(string name, string? phone, string ?email)
+        {
+            var response = await _customerService.GetAllCustomerAsync(name ,phone,email);
+            return ProcessResponse(response);
+        }
+
+        // POST: api/customer
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] CustomerCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var response = await _customerService.CreateCustomerAsync(model);
+            return ProcessResponse(response);
+        }
+
+        // PUT: api/customer/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, [FromBody] CustomerCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var response = await _customerService.UpdateCustomerAsync(model);
+
+
+            return ProcessResponse(response);
+        }
+
+        // DELETE: api/customer/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _customerService.DeleteCustomerAsync(id);
+
+
+            return ProcessResponse(response);
+        }
+    }
+
+}
