c00a37cae77b5f1d52b6b683d434a033510adc49
randafci0 <randafci0@gmail.com>
2025-08-23 21:27:22 +0300

    fix

---
+++ Ontime.Module.Employee.Service/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace OnTim.Employee.Services.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+         
+            
+
+        }
+    }
+}
---
+++ Ontime.Module.Employee.Service/ModuleServicesDependences.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace OnTime.Employee.Services
+{
+    public static class ModuleServicesDependences
+    {
+        public static IServiceCollection AddReposetoriesServices(this IServiceCollection service)
+        {
+           
+
+
+
+
+            return service;
+        }
+    }
+}
